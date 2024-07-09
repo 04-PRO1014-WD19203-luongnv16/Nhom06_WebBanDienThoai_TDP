147c7bc8f8f82becfc2740186a198beeaeb5716b
phinv <phinvph46574@fpt.edu.vn>
2024-07-09 21:13:14 +0700

    tesst

---
+++ taikhoan/dangky.php
@@ -0,0 +1,67 @@
+<div class="header_container">
+        <div class="header_menu1">
+            <ul>
+                <li><a href=""><h4>Trang Chủ</h4></a></li>
+                <li><a href=""><h4>Giới Thiệu</h4></a></li>
+                <li><a href=""><h4>Chi Nhánh</h4></a>
+                </li>
+            </ul>
+        </div>
+        <div class="header_logo">
+            <a href="#"><img class="logo_header" src="img/logo slider.png"></a>
+        </div>
+        <div class="header_menu2">
+            <ul>
+                <li><a href=""><h4>Liên Hệ</h4></a></li>
+                <li><a href=""><h4>Tuyển Dụng</h4></a></li>
+                <li><a href=""><h4>Tạo Tài Khoản</h4></a></li>
+            </ul>
+        </div>
+    </div>
+
+<br>
+
+
+<div class="boxtitle">ĐĂNG KÍ TÀI KHOẢN</div>
+<div class="dangki_container">
+   
+         <div class=" row boxcontent1 formtaikhoan">
+     <form action="" method="">
+
+        <div class="row ">
+           Email <br>
+        <input type="text" name="">
+        </div>
+
+        <div class="row ">
+            Tên đăng nhập <br>
+        <input type="text" name="">
+        </div>
+
+        <div class="row ">
+            Mật khẩu <br>
+        <input type="password" name="">
+        </div>
+
+        <div class="row ">
+            Địa chỉ <br>
+        <input type="text" name="">
+        </div>  
+    
+        <div class="row">
+            Số điện thoại <br>
+        <input type="text" name="">
+        </div> 
+
+        <input type="submit" value="Đăng kí" >
+        
+     
+    </form>
+         </div>
+         
+      <div class="anh_dangki">
+        <img class="dangki_anh" src="img/dangki.webp" alt="">
+      </div>
+     
+        </div>
+</div>
