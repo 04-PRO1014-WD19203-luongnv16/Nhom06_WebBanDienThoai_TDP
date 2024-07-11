7ef0f7682931874d317ceb81b21fe51c24e68e18
Nguyenngocthang1234 <thangbek123@gmail.com>
2024-07-11 18:00:12 +0700

    update admin

--- model/danhmuc.php
+++ model/danhmuc.php
@@ -1,25 +1,23 @@
 <?php
-function insert_danhmuc($tenloai){
-    $sql="insert into danhmuc(name) values('$tenloai')";
+  function loadAll_danhmuc(){
+    $sql = "select * from danhmuc";
+    return pdo_query($sql);
+  }
+  function insert_danhmuc($name){
+    $sql = "INSERT INTO danhmuc (name) VALUES ('$name')";
     pdo_execute($sql);
-}
-function delete_danhmuc($id){
-    $sql="delete from danhmuc where id=".$id;
+  }
+  function delete_danhmuc($id){
+    $sql = "DELETE FROM danhmuc WHERE id =" .$id;
     pdo_execute($sql);
-}
-function loadall_danhmuc(){
-    $sql="select * from danhmuc order by id desc";
-    $listdanhmuc=pdo_query($sql);
-    return $listdanhmuc;
-}
-function loadone_danhmuc($id){
-    $sql="select * from danhmuc where id=".$id;
-    $dm=pdo_query_one($sql);
+  }
+  function loadOne_danhmuc($id){
+    $sql = "SELECT * FROM danhmuc WHERE id =" .$id;
+    $dm = pdo_query_one($sql);
     return $dm;
-}
-function update_danhmuc($id,$tenloai){
-    $sql="update danhmuc set name='".$tenloai."' where id=".$id;
+  }
+  function update_danhmuc($id,$name){
+    $sql = "UPDATE danhmuc SET name = '$name' WHERE id =" .$id;
     pdo_execute($sql);
-}
-
+  } 
 ?>
--- model/sanpham.php
+++ model/sanpham.php
@@ -1,60 +1,28 @@
 <?php
-function insert_sanpham($tensp,$giasp,$hinh,$mota,$iddm){
-    $sql="insert into sanpham(name,price,img,mota,iddm) values('$tensp','$giasp','$hinh','$mota','$iddm')";
+function insert_sanpham($name,$mota,$img,$import_price,$sale_price,$listed_price,$stock,$iddm){
+  $sql = "INSERT INTO sanpham(name,mota,img,import_price,sale_price,listed_price,stock,iddm) values('$name','$mota','$img','$import_price','$sale_price','$listed_price',$stock,'$iddm')";
+  pdo_execute($sql);
+}
+  function loadAll_sanpham(){
+    $sql = "SELECT sanpham.*, danhmuc.name FROM sanpham inner join danhmuc on sanpham.iddm = danhmuc.id order by sanpham.id desc";
+    $listsanpham=pdo_query($sql);
+    return $listsanpham;
+  }
+  function delete_sanpham($id){
+    $sql = "DELETE FROM sanpham WHERE id=".$id;
     pdo_execute($sql);
-}
-function delete_sanpham($id){
-    $sql="delete from sanpham where id=".$id;
-    pdo_execute($sql);
-}
-function loadall_sanpham_top10(){
-    $sql="select * from sanpham where 1 order by luotxem desc limit 0,10";
-    $listsanpham=pdo_query($sql);
-    return $listsanpham;
-}
-function loadall_sanpham_home(){
-    $sql="select * from sanpham where 1 order by id desc limit 0,9";
-    $listsanpham=pdo_query($sql);
-    return $listsanpham;
-}
-function loadall_sanpham($kyw="", $iddm=0){
-    $sql="select * from sanpham where 1";
-    if($kyw!=""){
-        $sql.=" and name like '%".$kyw."%'";
-    }
-    if($iddm>0){
-        $sql.=" and iddm ='".$iddm."'";
-    }
-    $sql.=" order by id desc";
-    $listsanpham=pdo_query($sql);
-    return $listsanpham;
-}
-function loadone_ten_dm($iddm){
-    if($iddm>0){
-    $sql="select * from danhmuc where id=".$iddm;
-    $dm=pdo_query_one($sql);
-    extract($dm);
-    return $name;
-    }else{
-        return "";
-    }
-}
-function loadone_sanpham($id){
-    $sql="select * from sanpham where id=".$id;
-    $sp=pdo_query_one($sql);
+  }
+  function loadOne_sanpham($id){
+    $sql = "SELECT * FROM sanpham WHERE id=".$id;
+    $sp = pdo_query_one($sql);
     return $sp;
-}
-function load_sanpham_cungloai($id,$iddm){
-    $sql="select * from sanpham where iddm=".$iddm." AND id <>".$id;
-    $listsanpham=pdo_query($sql);
-    return $listsanpham;
-}
-function update_sanpham($id,$iddm,$tensp,$giasp,$mota,$hinh){
-    if($hinh!="")
-       $sql="update sanpham set iddm='".$iddm."',name='".$tensp."',price='".$giasp."',mota='".$mota."',img='".$hinh."' where id=".$id;
-    else
-       $sql="update sanpham set iddm='".$iddm."',name='".$tensp."',price='".$giasp."',mota='".$mota."' where id=".$id;
+  }
+  function update_sanpham($id,$name,$mota,$img,$import_price,$sale_price,$listed_price,$stock,$iddm){
+    if($img!=""){
+      $sql = "UPDATE sanpham SET name='$name',mota='$mota',img='$img',import_price='$import_price',sale_price='$sale_price',listed_price='$listed_price',stock='$stock',iddm='$iddm' WHERE id=".$id;
+    }else{
+      $sql = "UPDATE sanpham SET name='$name',mota='$mota',import_price='$import_price',sale_price='$sale_price',listed_price='$listed_price',stock='$stock',iddm='$iddm' WHERE id=".$id;
+    }
     pdo_execute($sql);
-}
-
-?>
+  }
+?> 
